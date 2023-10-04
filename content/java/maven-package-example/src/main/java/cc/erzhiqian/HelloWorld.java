28f518344c7dbacef104bdc18d48e5f80e00ad8e
erzhiqianyi <github@erzhiqian.cc>
2023-10-04 19:51:59 +0800

    add xchart example

---
+++ content/java/maven-package-example/src/main/java/cc/erzhiqian/HelloWorld.java
@@ -0,0 +1,28 @@
+package cc.erzhiqian;
+
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+
+import java.io.IOException;
+
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+
+        double[] xData = new double[]{0.0, 1.0, 2.0};
+        double[] yData = new double[]{2.0, 1.0, 0.0};
+
+        // Create Chart
+        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+
+        // Show it
+        new SwingWrapper(chart).displayChart();
+
+        // Save it
+        BitmapEncoder.saveBitmap(chart, "./Sample_Chart", BitmapEncoder.BitmapFormat.PNG);
+
+        // or save it in high-res
+        BitmapEncoder.saveBitmapWithDPI(chart, "./Sample_Chart_300_DPI", BitmapEncoder.BitmapFormat.PNG, 300);
+    }
+}
